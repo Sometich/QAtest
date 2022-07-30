95884cfe433c561a0edffd957529261d2115f23a
Алексей <76786794+Sometich@users.noreply.github.com>
2022-07-30 23:30:40 +0300

    Update Main.java

--- exercise1/Main.java
+++ exercise1/Main.java
@@ -1,18 +1,47 @@
-package com.example.testpostman;
 
+import java.util.Scanner;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+public class Main {
 
-@RestController
-public class Controller {
-
-    @GetMapping("/name/{id}")
-    public String getName(@PathVariable("id") String id) {
-        if (id.equals("2")) {
-            return "Correct";
+    /**
+     * Программа вычисляет числа фибоначи
+     * На вход программа принимает индекс n
+     * С помощью в зависимости от отрицательного или положительного значения программа рассчитывает путем рекурсии
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите порядковый номер числа Фибоначи которое вы хотите вывести");
+        try {
+            long n = scanner.nextLong();
+            if (n == 0) {
+                System.out.printf("Число с порядковым номером %d равно %d\n", n, 0);
+            } else if (n == 1) {
+                System.out.printf("Число с порядковым номером %d равно %d\n", n, 1);
+            } else if (n < 0) {
+                //Вычисление числа Фибоначи с отрицательным индексом
+                System.out.printf("Число с порядковым номером %d равно %d\n", n, fibonachiNumber(n - 1));
+            } else {
+                //Вычисление числа Фибоначи с положительным индексом
+                System.out.printf("Число с порядковым номером %d равно %d\n", n, fibonachiNumber(n + 1));
+            }
+        } catch (Exception e) {
+            System.out.println("Некорректный ввод");
         }
-        return "error";
+    }
+
+    public static long fibonachiNumber(long n) {
+        if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else if (n == -1) {
+            return 1;
+        } else if (n > 0) {
+            return fibonachiNumber(n - 1) + fibonachiNumber(n - 2);
+        } else {
+            //Для случая с отрицательным числом
+            int sign = n % 2 == 0 ? 1 : -1;
+            return sign * fibonachiNumber(-n);
+        }
     }
 }
